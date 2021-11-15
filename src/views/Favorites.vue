f11fadceeb77818b0a481efa39f147bcf4e8000e
xz633 <xz633@cornell.edu>
2021-11-15 11:47:53 -0500

    favorite tab

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="favorites">
-    <h1> Favorites </h1>
+    <h1>Favorites</h1>
     <!-- <h2> Favorites | Created Recipes </h2>  -->
+    <div>
+      <b-tabs content-class="mt-3" fill>
+        <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+        <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+        <b-tab title="Very, very long title"
+          ><p>I'm the tab with the very, very long title</p></b-tab>
+        <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
