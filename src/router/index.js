57f877827ce5ef9ec9102d845ae7f5abc3ead043
ac2687 <ac2687@cornell.edu>
2021-11-14 21:00:40 -0500

    capitalized

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
   },
   {
     path: '/favorites',
-    name: 'favorites',
+    name: 'Favorites',
     // route level code-splitting
     // this generates a separate chunk (favorites.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
