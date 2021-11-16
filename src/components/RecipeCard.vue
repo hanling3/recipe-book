3f61d08f11ad3e039db34e750fcf24ba7b82caa3
Hanling Yang <hy378@cornell.edu>
2021-11-16 18:45:47 -0500

    added Recipe Card component

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="recipe card">
+    <b-row>
+      <b-col>
+          <img src="../assets/logo.png" class="rounded mx-auto d-block" alt="...">
+        <!-- {{ dishPic }} -->
+        dishPic
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col>
+        <!-- {{ dishName }} -->
+        dishName
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col>
+        <b-icon icon="clock" scale="1"></b-icon>
+      </b-col>
+      <b-col>
+        <!-- {{ dishCookTime }} -->
+      </b-col>
+      <b-col>
+        <!-- {{ dishCost }} -->
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recipeCard",
+  props: {
+      dishPic: String, 
+      dishName: String,
+      dishCookTime: Number,
+      dishCost: Number
+  },
+};
+</script>
