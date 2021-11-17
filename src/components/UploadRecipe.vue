0838080c49bfa9da4cfde6188b36082a74cdcc0e
xz633 <xz633@cornell.edu>
2021-11-17 00:25:10 -0500

    upload recipe

--- src/components/UploadRecipe.vue
+++ src/components/UploadRecipe.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="col-lg-6 col-sm-8">
-      <h1>Create Your recipe</h1>
+  <div class="col-lg-6 col-sm-8 justify-content-center container">
+    <h1>Create Your recipe</h1>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-
-      <b-form-group id="input-group-1" label="Dish Name:" label-size="lg" label-for="input-1" >
+      <b-form-group
+        id="input-group-1"
+        label="Dish Name:"
+        label-size="lg"
+        label-for="input-1"
+        class="mt-3"
+      >
         <b-form-input
           id="input-1"
           v-model="form.name"
@@ -12,9 +17,15 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Time Duration:" label-size="lg" label-for="input-2">
+      <b-form-group
+        id="input-group-2"
+        label="Time Duration:"
+        label-size="lg"
+        label-for="input-2"
+        class="mt-3"
+      >
         <b-form-select
-        class="col-12"
+          class="col-12"
           id="input-2"
           v-model="form.time"
           :options="time"
@@ -22,32 +33,67 @@
         ></b-form-select>
       </b-form-group>
 
-     <b-form-group id="input-group-3" label="Ingredient:" label-size="lg" label-for="input-3" inline>
-<b-form-input
-          id="input-3"
-          v-model="form.step"
-          placeholder="Enter step"
-          required
-        ></b-form-input>
-      <b-form-input
-          id="input-3"
-          v-model="form.step"
-          placeholder="Enter step"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-4" label="Step:" label-size="lg" label-for="input-4">
+    <b-form-group
+        id="input-group-3"
+        label="Ingredient:"
+        label-size="lg"
+        label-for="input-3"
+        inline
+        class="mt-3"
+      >
+      <b-row>
+        <b-col>
+          <b-form-group
+        id="input-group-3"
+        label="Item:"
+      >
         <b-form-input
-          id="input-1"
-          v-model="form.step"
-          placeholder="Enter step"
+          id="input-3"
+          v-model="form.ingredient[item]"
+          placeholder="Enter item"
           required
         ></b-form-input>
+        </b-form-group>
+        </b-col>
+          <b-col>
+          <b-form-group
+        id="input-group-3"
+        label="Ratio:"
+      >
+        <b-form-input
+          id="input-3"
+          v-model="form.ingredient[ratio]"
+          placeholder="Enter ratio"
+          required
+        ></b-form-input>
+        </b-form-group>
+        </b-col>
+        </b-row>
+</b-form-group>
+
+      <b-form-group
+        id="input-group-1"
+        label="Step:"
+        label-size="lg"
+        label-for="input-1"
+        class="mt-3"
+      >
+        <b-form-input
+          id="input-4"
+          v-model="form.step"
+          placeholder="Step"
+          required
+        ></b-form-input>
+        <b-button v-on:click="addStep">Add Step</b-button>
       </b-form-group>
 
-
-      <b-form-group id="input-group-5" label="Additional tip:" label-size="lg" label-for="input-5">
+      <b-form-group
+        id="input-group-5"
+        label="Additional tip:"
+        label-size="lg"
+        label-for="input-4"
+        class="mt-3"
+      >
         <b-form-textarea
           id="textarea"
           v-model="form.tip"
@@ -70,13 +116,21 @@
 export default {
   data() {
     return {
+      item:"",
+      ratio:"",
       form: {
         name: "",
         time: null,
-        step:"",
+        ingredient:[],
+        step: "",
         tip: "",
       },
-      time: [{ text: 'Select One', value: null }, '<30min', '<1 hour', '>= 1 hour'],
+      time: [
+        { text: "Select One", value: null },
+        "<30min",
+        "<1 hour",
+        ">= 1 hour",
+      ],
       show: true,
     };
   },
@@ -90,13 +144,20 @@ export default {
       // Reset our form values
       this.form.name = "";
       this.form.tip = "";
-      this.form.time = null
+      this.form.time = null;
       // Trick to reset/clear native browser form validation state
       this.show = false;
       this.$nextTick(() => {
         this.show = true;
       });
     },
+    addItem(){
+    
+
+    },
+    addStep(){
+
+    }
   },
 };
 </script>
