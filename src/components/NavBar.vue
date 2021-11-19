6835f5f8a972ea967879a53f64906cf590a39319
ac2687 <ac2687@cornell.edu>
2021-11-18 23:06:54 -0500

    Added navbar, profile page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,21 +2,21 @@
   <div id="nav">
     <b-card id="navButton">
       <router-link to="/mealplan" tag="button">
-        <b-icon icon="calendar2-week"></b-icon>
+        <b-icon icon="calendar2-week" class="icons"></b-icon>
         <p>Plan</p>
       </router-link>
     </b-card>
 
     <b-card id="navButton">
       <router-link to="/Favorites" tag="button">
-        <b-icon icon="heart"></b-icon>
+        <b-icon icon="heart" class="icons"></b-icon>
         <p>Saved</p>
       </router-link>
     </b-card>
 
     <b-card id="navButton">
       <router-link to="/Profile" tag="button">
-        <b-icon icon="person"></b-icon>
+        <b-icon icon="person" class="icons"></b-icon>
         <p>Profile</p>
       </router-link>
     </b-card>
@@ -30,8 +30,23 @@ export default {
 </script>
 
 <style scoped>
+
+#nav {
+  text-align: center;
+  background-color: #d4d3d3;
+}
+
+.icons {
+  width: 50px;
+  height: 50px;
+}
+
 #navButton {
   display: inline-flex;
   border: none;
+  text-align: center;
+  background-color: #d4d3d3;
 }
+
+
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,58 +2,138 @@
   <div class="profile">
     <h1>Profile </h1>
 
+    <div class = "header"> 
+      <div> <img src="@/assets/profile_propic.jpg" alt="Robo the persona"> </div>
+      <div> <h2> Robo  <b-icon icon="pencil"></b-icon> </h2> </div>
+    </div>
 
-    <b-icon icon="person"></b-icon> 
-    <h2> Robo  <b-icon icon="pencil"></b-icon> </h2> 
+    <div class = "badge-section">
+      <h4> My Badges </h4> 
+      <div class = "badges">
+        <div> 
+          <b-button class="circle"> <b-icon icon="star" class="star"></b-icon> </b-button>
+          <h5> Newly Joined </h5>
+        </div>
 
-  
+        <div>   
+          <b-button class="circle"> <b-icon icon="star" class="star"></b-icon> </b-button>
+          <h5> Consistent Cook </h5>
+        </div>
 
-    <div class = "badges">
-      <h4> My Badges </h4>
-      <b-button class="b"> Newly Joined </b-button>
-      <b-button class="b"> Consistent Cook </b-button>
-      <b-button class="b"> Masterchef </b-button>
-    </div> 
+        <div>
+          <b-button class="circle"> <b-icon icon="star" class="star"></b-icon> </b-button>
+          <h5> Masterchef </h5>
+        </div> 
+      </div>  
+    </div>
 
     <div class="report">
       <h4> Cooking Report </h4>
     <ul> 
-      <li> <p class="left"> Followed 5 recipes this week </p> </li>
-      <li> <p class="left"> Spend $100 on food </p> </li> 
-      <li> <p class="left"> Saved 15 recipes </p> </li>
+      <li> <p class="left"> Completed 5 recipes this week </p> </li>
+      <li> <p class="left"> Saved $100 on food by cooking compared to grabbing takeout </p> </li> 
+      <li> <p class="left"> Saved 5 hours cooking throughout the week </p> </li>
     </ul>  
     </div>
+    <nav-bar />
   </div>
 </template>
 
 <script>
+import navBar from "../components/NavBar.vue";
+
+export default {
+  name: "profile",
+  components: {
+    navBar
+  },
+  data: () => ({
+
+  })
+}
+
 </script>
 
 <style>
 
 h1 {
   text-align: left;
+  margin-left: 50px;
+  margin-top: 50px;
+}
+
+/* Header */
+
+img {
+  margin-bottom: 30px;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.p-icon-size {
+  position: absolute;
+  left: 38%;
+  height: 100px;
+  width: 100px;
 }
 
 h2 {
   font-weight: bold;
 }
 
-.badges {
-  margin: 50px;
+/* Badges */
+
+h4 {
+  margin: 0 auto;
+  font-weight: bold;
+}
+
+.circle {
+  /* position: absolute; */
+  /* top: 38%; */
+  text-align: center;
+  margin: 20px;
+  height: 100px;
+  width: 100px;
   border-radius: 100%;
   font-weight: bold;
   font-size: 20px;
-  width: 50px; 
+  /* padding: 20px; */
+  /* width: 50px;  
   margin: 5px; 
-  padding: 25px; 
-  
+  padding: 25px; */ 
 }
 
+.star {
+  height: 50px;
+  width: 50px;
+}
+
+.badges {
+  text-align: center;
+  margin: auto;
+  width: 30%;  
+  display: flex;
+  flex-direction: row;
+} 
+
+.badge-section {
+  text-align: center;
+  /* margin: 0 auto; */
+}
+
+/* Report */
+
 .report {
+  text-align: center;
   border: 1px solid #d6d6d6;
-  padding: 10px;
-  margin: 20px;
+  padding: 20px;
+  margin: 50px;
 }
 
 .left {
