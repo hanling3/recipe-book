f0bfb77586ab05de96a60b00c5459c302260fb58
Hanling Yang <hy378@cornell.edu>
2021-11-23 00:18:33 -0500

    updated recipe detail page

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,32 +1,37 @@
 <template>
-  <!-- <router-link :to='require("/recipe-detail/" + dish.dish)'> -->
-  <router-link to="/recipe-detail/kung-pao-chicken">
     <div id="recipeCard">
+      <!-- <router-link 
+        :to="'/recipe-detail/' + dish.reference"
+        v-on: click="$emit('update-dish', 'kung-pao-chicken')"
+        > -->
+       <router-link to="/recipe-detail/kung-pao-chicken">
       <b-card
         :title="dish.dish"
         title-tag="h5"
-        :img-src="require('../assets/' + dish.image)"
+        :img-src="require('../assets/' + dish.reference + '.jpg')"
         img-alt="Image"
         img-top
-        img-height=150rem
-        style="width: 15rem"  
-        class="nowrap d-flex justify-content-center" 
+        img-height="150rem"
+        style="width: 15rem"
+        class="nowrap d-flex justify-content-center"
       >
         <b-card-text class="d-flex justify-content-between">
-          <!-- <img id="dishImage" :src='require("../assets/" + dish.image)'> -->
-          <div> 
+          <div>
             <b-icon icon="clock" scale="1"></b-icon>
             {{ dish.cooktime }}
           </div>
           <div>
-          <b-icon icon="cash-stack" scale="1"></b-icon>
-          {{ dish.budget }}
+            <b-icon icon="cash-stack" scale="1"></b-icon>
+            {{ dish.budget }}
           </div>
-          {{ dish.difficulty }}
+          <div>
+            <b-icon icon="star" scale="1"></b-icon>
+            {{ dish.difficulty }}
+          </div>
         </b-card-text>
       </b-card>
+       </router-link>
     </div>
-  </router-link>
 </template>
 
 <script>
@@ -51,7 +56,5 @@ a {
 #recipeCard {
   display: inline-flex;
   width: 300px;
-
 }
-
 </style>
